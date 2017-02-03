6790a4bce2b5689c7f14efc10615aada8d4a10d2
gavy41 <409733284@qq.com>
2017-02-03 20:52:52 +0800

    sql

--- yblog_admin_user.sql
+++ yblog_admin_user.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
+Source Server         : 本地
 Source Server Version : 50617
 Source Host           : localhost:3306
 Source Database       : yblog
@@ -10,23 +10,26 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2017-01-22 16:13:57
+Date: 2017-02-03 20:52:08
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for yblog_admin_user
+-- Table structure for `yblog_admin_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `yblog_admin_user`;
 CREATE TABLE `yblog_admin_user` (
   `admin_id` int(8) NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(24) DEFAULT NULL,
   `admin_psw` varchar(64) DEFAULT NULL,
+  `auth_key` varchar(64) DEFAULT '',
   `group_id` tinyint(3) DEFAULT '0',
   PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of yblog_admin_user
 -- ----------------------------
+INSERT INTO `yblog_admin_user` VALUES ('39', 'gavy', 'cdd6f519c25265675ff0a86ad6a28d65', '8jsDIxjOso8OUdCUFK-oNPxU3YshP_5j', '2');
+INSERT INTO `yblog_admin_user` VALUES ('40', 'admin', '3e9fedb9521a4bc26b637da1c35020d7', 'f1mzLSf9Xox_e0qh5G9UOhrOUADw6krn', '1');
